d1642ad2a160f5ca46104b10d3537a97c690c179
Minwell <chenyimin@workspace.chenyimin.ws2.wh-a.brainpp.cn>
2019-12-24 15:07:01 +0800

    add logging for registering hooks

--- dmb/apis/train.py
+++ dmb/apis/train.py
@@ -10,6 +10,7 @@ from mmcv.parallel import MMDataParallel, MMDistributedDataParallel
 
 from dmb.utils.env import get_root_logger
 from dmb.data.loaders import build_data_loader
+from dmb.utils.solver import build_optimizer
 from dmb.data.datasets.evaluation.stereo import DistStereoEvalHook
 from dmb.utils import DistOptimizerHook, DistApexOptimizerHook
 from dmb.utils import TensorboardLoggerHook, TextLoggerHook
@@ -24,32 +25,32 @@ except ImportError:
     raise ImportError('Use APEX for multi-precision via apex.amp')
 
 
-def parse_losses(losses):
-    log_vars = OrderedDict()
-    for loss_name, loss_value in losses.items():
-        if isinstance(loss_value, torch.Tensor):
-            log_vars[loss_name] = loss_value.mean()
-        elif isinstance(loss_value, list):
-            log_vars[loss_name] = sum(_loss.mean() for _loss in loss_value)
-        else:
-            raise TypeError(
-                '{} is not a tensor or list of tensors'.format(loss_name))
-
-    loss = sum(_value for _key, _value in log_vars.items() if 'loss' in _key)
-
-    log_vars['loss'] = loss
-    for name in log_vars:
-        log_vars[name] = log_vars[name].item()
-
-    return loss, log_vars
-
-
 def batch_processor(model, data, train_mode):
     if train_mode:
         model.train()
     else:
         model.eval()
     _, losses = model(data)
+
+    def parse_losses(losses):
+        log_vars = OrderedDict()
+        for loss_name, loss_value in losses.items():
+            if isinstance(loss_value, torch.Tensor):
+                log_vars[loss_name] = loss_value.mean()
+            elif isinstance(loss_value, list):
+                log_vars[loss_name] = sum(_loss.mean() for _loss in loss_value)
+            else:
+                raise TypeError(
+                    '{} is not a tensor or list of tensors'.format(loss_name))
+
+        loss = sum(_value for _key, _value in log_vars.items() if 'loss' in _key)
+
+        log_vars['loss'] = loss
+        for name in log_vars:
+            log_vars[name] = log_vars[name].item()
+
+        return loss, log_vars
+
     loss, log_vars = parse_losses(losses)
 
     outputs = dict(
@@ -62,86 +63,26 @@ def batch_processor(model, data, train_mode):
 
 def train_matcher(
         cfg, model, train_dataset,
-        eval_dataset=None, test_dataset=None,
+        eval_dataset=None, vis_dataset=None,
         distributed=False, validate=False, logger=None
 ):
     if logger is None:
         logger = get_root_logger(cfg.log_level)
 
+    import pdb
+    pdb.set_trace()
+
     # start training
     if distributed:
-        _dist_train(model, train_dataset, cfg, eval_dataset, test_dataset, validate=validate, logger=logger)
+        _dist_train(model, train_dataset, cfg, eval_dataset, vis_dataset, validate=validate, logger=logger)
     else:
-        _non_dist_train(model, train_dataset, cfg, eval_dataset, test_dataset, validate=validate, logger=logger)
+        _non_dist_train(model, train_dataset, cfg, eval_dataset, vis_dataset, validate=validate, logger=logger)
 
 
-def build_optimizer(model, optimizer_cfg):
-    """Build optimizer from configs.
-    Args:
-        model (:obj:`nn.Module`): The model with parameters to be optimized.
-        optimizer_cfg (dict): The config dict of the optimizer.
-            Positional fields are:
-                - type: class name of the optimizer.
-                - lr: base learning rate.
-            Optional fields are:
-                - any arguments of the corresponding optimizer type, e.g.,
-                  weight_decay, momentum, etc.
-                - paramwise_options: a dict with 3 accepted fileds
-                  (bias_lr_mult, bias_decay_mult, norm_decay_mult).
-                  `bias_lr_mult` and `bias_decay_mult` will be multiplied to
-                  the lr and weight decay respectively for all bias parameters
-                  (except for the normalization layers), and
-                  `norm_decay_mult` will be multiplied to the weight decay
-                  for all weight and bias parameters of normalization layers.
-    Returns:
-        torch.optim.Optimizer: The initialized optimizer.
-    """
-    if hasattr(model, 'module'):
-        model = model.module
-
-    optimizer_cfg = optimizer_cfg.copy()
-    paramwise_options = optimizer_cfg.pop('paramwise_options', None)
-    # if no paramwise option is specified, just use the global setting
-    if paramwise_options is None:
-        return obj_from_dict(optimizer_cfg, torch.optim, dict(params=model.parameters()))
-    else:
-        assert isinstance(paramwise_options, dict)
-        # get base lr and weight decay
-        base_lr = optimizer_cfg['lr']
-        base_wd = optimizer_cfg.get('weight_decay', None)
-        # weight_decay must be explicitly specified if mult is specified
-        if ('bias_decay_mult' in paramwise_options or 'norm_decay_mult' in paramwise_options):
-            assert base_wd is not None
-        # get param-wise options
-        bias_lr_mult = paramwise_options.get('bias_lr_mult', 1.)
-        bias_decay_mult = paramwise_options.get('bias_decay_mult', 1.)
-        norm_decay_mult = paramwise_options.get('norm_decay_mult', 1.)
-        # set param-wise lr and weight decay
-        params = []
-        for name, param in model.named_parameters():
-            if not param.requires_grad:
-                continue
-
-            param_group = {'params': [param]}
-            # for norm layers, overwrite the weight decay of weight and bias
-            # TODO: obtain the norm layer prefixes dynamically
-            if re.search(r'(bn|gn)(\d+)?.(weight|bias)', name):
-                if base_wd is not None:
-                    param_group['weight_decay'] = base_wd * norm_decay_mult
-            # for other layers, overwrite both lr and weight decay of bias
-            elif name.endswith('.bias'):
-                param_group['lr'] = base_lr * bias_lr_mult
-                if base_wd is not None:
-                    param_group['weight_decay'] = base_wd * bias_decay_mult
-            # otherwise use the global settings
-
-            params.append(param_group)
-
-        optimizer_cls = getattr(torch.optim, optimizer_cfg.pop('type'))
-        return optimizer_cls(params, **optimizer_cfg)
-
-
-def _dist_train(model, train_dataset, cfg, eval_dataset=None, vis_dataset=None, validate=False, logger=None):
+def _dist_train(
+        model, train_dataset, cfg,
+        eval_dataset=None, vis_dataset=None, validate=False, logger=None
+):
     # prepare data loaders
     data_loaders = [
         build_data_loader(
@@ -168,16 +109,17 @@ def _dist_train(model, train_dataset, cfg, eval_dataset=None, vis_dataset=None, 
 
     # put model on gpus
     model = MMDistributedDataParallel(model)
-
     # build runner
     runner = Runner(
         model, batch_processor, optimizer, cfg.work_dir, cfg.log_level, logger
     )
-    # register hooks
+
+    # register optimizer hooks
     if cfg.apex.use_mixed_precision:
         optimizer_config = DistApexOptimizerHook(**cfg.optimizer_config)
     else:
         optimizer_config = DistOptimizerHook(**cfg.optimizer_config)
+    logger.info("Register Optimizer Hook...")
     runner.register_training_hooks(
         cfg.lr_config, optimizer_config, cfg.checkpoint_config, log_config=None
     )
@@ -186,17 +128,21 @@ def _dist_train(model, train_dataset, cfg, eval_dataset=None, vis_dataset=None, 
     for info in cfg.log_config['hooks']:
         assert isinstance(info, dict) and 'type' in info
         if info['type'] in ['TensorboardLoggerHook']:
+            logger.info("Register Tensorboard Logger Hook...")
             runner.register_hook(
                 TensorboardLoggerHook(interval=cfg.log_config.interval, register_logWithIter_keyword=['loss']),
                 priority='VERY_LOW'
             )
         if info['type'] in ['TextLoggerHook']:
+            logger.info("Register Text Logger Hook...")
             runner.register_hook(
                 TextLoggerHook(interval=cfg.log_config.interval, ),
                 priority='VERY_LOW'
             )
 
+    logger.info("Register SamplerSeed Hook...")
     runner.register_hook(DistSamplerSeedHook())
+    logger.info("Register EmptyCache Hook...")
     runner.register_hook(
         EmptyCacheHook(before_epoch=True, after_iter=False, after_epoch=True),
         priority='VERY_LOW'
@@ -206,8 +152,10 @@ def _dist_train(model, train_dataset, cfg, eval_dataset=None, vis_dataset=None, 
     if validate:
         interval = cfg.get('validate_interval', 1)
         if eval_dataset is not None:
+            logger.info("Register Evaluation Hook...")
             runner.register_hook(DistStereoEvalHook(eval_dataset, interval))
         if vis_dataset is not None:
+            logger.info("Register Visulization hook...")
             runner.register_hook(DistStereoVisHook(vis_dataset, cfg, interval))
 
     if cfg.resume_from:
@@ -218,7 +166,10 @@ def _dist_train(model, train_dataset, cfg, eval_dataset=None, vis_dataset=None, 
     runner.run(data_loaders, cfg.workflow, cfg.total_epochs)
 
 
-def _non_dist_train(model, train_dataset, cfg, eval_dataset=None, vis_dataset=None, validate=False, logger=None):
+def _non_dist_train(
+        model, train_dataset, cfg,
+        eval_dataset=None, vis_dataset=None, validate=False, logger=None
+):
     # prepare data loaders
     data_loaders = [
         build_data_loader(
@@ -235,9 +186,11 @@ def _non_dist_train(model, train_dataset, cfg, eval_dataset=None, vis_dataset=No
     runner = Runner(
         model, batch_processor, optimizer, cfg.work_dir, cfg.log_level, logger
     )
+    logger.info("Register Optimizer Hook...")
     runner.register_training_hooks(
         cfg.lr_config, cfg.optimizer_config, cfg.checkpoint_config, cfg.log_config
     )
+    logger.info("Register EmptyCache Hook...")
     runner.register_hook(
         EmptyCacheHook(before_epoch=True, after_iter=False, after_epoch=True),
         priority='VERY_LOW'
---
+++ tools/test.py
@@ -0,0 +1,265 @@
+import argparse
+import os
+import os.path as osp
+import shutil
+import tempfile
+
+import numpy as np
+from imageio import imread
+
+import torch
+import torch.distributed as dist
+
+import mmcv
+from mmcv import mkdir_or_exist
+from mmcv.runner import LogBuffer
+from mmcv.runner import load_checkpoint, get_dist_info
+from mmcv.parallel import MMDataParallel, MMDistributedDataParallel
+from mmcv.parallel import scatter, collate
+
+from dmb.utils.collect_env import collect_env_info
+from dmb.utils.env import init_dist, get_root_logger
+from dmb.modeling.stereo import build_stereo_model as build_model
+from dmb.data.datasets.stereo import build_dataset
+from dmb.apis.inference import save_result
+from dmb.visualization.stereo import sparsification_plot
+
+
+def sparsification_eval(result, cfg):
+    if hasattr(cfg, 'sparsification_plot') and cfg.sparsification_plot.doing:
+        if 'Confidence' in result and isinstance(result['Confidence'][0], torch.Tensor):
+            estConf = result['Confidence'][0].clone()
+        if 'GroundTruth' in result and isinstance(result['GroundTruth'], torch.Tensor):
+            gtDisp = result['GroundTruth'].clone()
+        if 'Disparity' in result and isinstance(result['Disparity'][0], torch.Tensor):
+            estDisp = result['Disparity'][0].clone()
+
+        if all([torch.is_tensor(estConf), torch.is_tensor(gtDisp),
+                torch.is_tensor(estDisp), isinstance(cfg.sparsification_plot.bins, int)]):
+            error_dict = sparsification_plot(
+                estDisp, gtDisp, estConf, cfg.sparsification_plot.bins,
+                cfg.model.eval.lower_bound, cfg.model.eval.upper_bound)
+
+            return error_dict
+
+
+def single_gpu_test(model, dataset, cfg, show=False):
+    return NotImplementedError
+
+
+def multi_gpu_test(model, dataset, cfg, show=False, tmpdir=None):
+    model.eval()
+    results = []
+    rank, world_size = get_dist_info()
+    if rank == 0:
+        prog_bar = mmcv.ProgressBar(len(dataset))
+
+    for idx in range(rank, len(dataset), world_size):
+        data = dataset[idx]
+
+        # None type data cannot be scatter, here we pick out the not None type data
+        notNoneData = {}
+        for k, v in zip(data.keys(), data.values()):
+            if v is not None:
+                notNoneData[k] = v
+        notNoneData = scatter(
+            collate([notNoneData], samples_per_gpu=1),
+            [torch.cuda.current_device()]
+        )[0]
+
+        data.update(notNoneData)
+
+        with torch.no_grad():
+            result = model(data)
+
+        filter_result = {}
+        filter_result.update(Error=result['Error'])
+
+        if show:
+            item = dataset.data_list[idx]
+            result['leftImage'] = imread(
+                osp.join(cfg.data.test.data_root, item['left_image_path'])
+            ).astype(np.float32)
+            result['rightImage'] = imread(
+                osp.join(cfg.data.test.data_root, item['right_image_path'])
+            ).astype(np.float32)
+            image_name = item['left_image_path'].split('/')[-1]
+            save_result(result, cfg.out_dir, image_name)
+
+        if hasattr(cfg, 'sparsification_plot'):
+            filter_result['Error'].update(sparsification_eval(result, cfg))
+
+        results.append(filter_result)
+
+        if rank == 0:
+            batch_size = world_size
+            for _ in range(batch_size):
+                prog_bar.update()
+
+    # collect results from all ranks
+    results = collect_results(results, len(dataset), tmpdir)
+
+    return results
+
+
+def collect_results(result_part, size, tmpdir=None):
+    rank, world_size = get_dist_info()
+    # create a tmp dir if it is not specified
+    if tmpdir is None:
+        MAX_LEN = 512
+        # 32 is whitespace
+        dir_tensor = torch.full((MAX_LEN,),
+                                32,
+                                dtype=torch.uint8,
+                                device='cuda')
+        if rank == 0:
+            tmpdir = tempfile.mkdtemp()
+            tmpdir = torch.Tensor(bytearray(tmpdir.encode()), dtype=torch.uint8, device='cuda')
+            dir_tensor[:len(tmpdir)] = tmpdir
+        dist.broadcast(dir_tensor, 0)
+        tmpdir = dir_tensor.cpu().numpy().tobytes().decode().rstrip()
+    else:
+        mmcv.mkdir_or_exist(tmpdir)
+    # dump the part result to the dir
+    mmcv.dump(result_part, osp.join(tmpdir, 'part_{}.pkl'.format(rank)))
+    dist.barrier()
+    # collect all parts
+    if rank != 0:
+        return None
+    else:
+        # load results of all parts from tmp dir
+        part_list = []
+        for i in range(world_size):
+            part_file = osp.join(tmpdir, 'part_{}.pkl'.format(i))
+            part_list.append(mmcv.load(part_file))
+        # sort the results
+        ordered_results = []
+        for res in zip(*part_list):
+            ordered_results.extend(list(res))
+        # the dataloader may pad some samples
+        ordered_results = ordered_results[:size]
+        # remove tmp dir
+        shutil.rmtree(tmpdir)
+        return ordered_results
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Test dense matching benchmark')
+    parser.add_argument('config', help='train config file path')
+    parser.add_argument('--checkpoint', help='checkpoint file')
+    parser.add_argument('--out_dir', help='output result directory')
+    parser.add_argument('--show', action='store_true', help='show results in images')
+    parser.add_argument(
+        '--evaluate',
+        action='store_true',
+        help='whether to evaluate the result')
+    parser.add_argument(
+        '--gpus',
+        type=int,
+        default=1,
+        help='number of gpus to use '
+             '(only applicable to non-distributed training)'
+    )
+    parser.add_argument(
+        '--launcher',
+        choices=['none', 'pytorch', 'slurm', 'mpi'],
+        default='pytorch',
+        help='job launcher'
+    )
+    parser.add_argument('--local_rank', type=int, default=0)
+
+    parser.add_argument('--out_path', help='needed by job client')
+    parser.add_argument('--in_path', help='needed by job client')
+    parser.add_argument('--pretrained_path', help='needed by job client')
+    parser.add_argument('--job_name', help='needed by job client')
+    parser.add_argument('--job_id', help='needed by job client')
+
+    args = parser.parse_args()
+    if 'LOCAL_RANK' not in os.environ:
+        os.environ['LOCAL_RANK'] = str(args.local_rank)
+
+    return args
+
+
+def main():
+    args = parse_args()
+
+    cfg = mmcv.Config.fromfile(args.config)
+    # set cudnn_benchmark
+    if cfg.get('cudnn_benchmark', False):
+        torch.backends.cudnn.benchmark = True
+
+    # init distributed env first, since logger depends on the dist info.
+    if args.launcher == 'none':
+        distributed = False
+    else:
+        distributed = True
+        init_dist(args.launcher, **cfg.dist_params)
+
+    if args.checkpoint is not None:
+        cfg.checkpoint = args.checkpoint
+    if args.out_dir is not None:
+        cfg.out_dir = args.out_dir
+    if args.gpus is not None:
+        cfg.gpus = args.gpus
+    cfg.show = args.show
+
+    mkdir_or_exist(cfg.out_dir)
+
+    # init logger before other step and setup training logger
+    logger = get_root_logger(cfg.out_dir, cfg.log_level, filename="test_log.txt")
+    logger.info("Using {} GPUs".format(cfg.gpus))
+    logger.info('Distributed training: {}'.format(distributed))
+
+    # log environment info
+    logger.info("Collecting env info (might take some time)")
+    logger.info("\n" + collect_env_info())
+
+    logger.info(args)
+
+    logger.info("Running with config:\n{}".format(cfg.text))
+
+    # build the dataset
+    test_dataset = build_dataset(cfg, 'test')
+
+    # build the model and load checkpoint
+    model = build_model(cfg)
+    checkpoint = load_checkpoint(model, cfg.checkpoint, map_location='cpu')
+
+    if not distributed:
+        model = MMDataParallel(model, device_ids=[0])
+        outputs = single_gpu_test(model, test_dataset, cfg, args.show)
+    else:
+        model = MMDistributedDataParallel(model.cuda())
+        outputs = multi_gpu_test(model, test_dataset, cfg, args.show, tmpdir=osp.join(cfg.out_dir, 'temp'))
+
+    rank, _ = get_dist_info()
+    if cfg.out_dir is not None and rank == 0:
+        result_path = osp.join(cfg.out_dir, 'result.pkl')
+        logger.info('\nwriting results to {}'.format(result_path))
+        mmcv.dump(outputs, result_path)
+
+        if args.evaluate:
+            error_log_buffer = LogBuffer()
+            for result in outputs:
+                error_log_buffer.update(result['Error'])
+            error_log_buffer.average()
+            log_items = []
+            for key in error_log_buffer.output.keys():
+
+                val = error_log_buffer.output[key]
+                if isinstance(val, float):
+                    val = '{:.4f}'.format(val)
+                log_items.append('{}: {}'.format(key, val))
+
+            if len(error_log_buffer.output) == 0:
+                log_items.append('nothing to evaluate!')
+
+            log_str = 'Evaluation Result: \t'
+            log_str += ', '.join(log_items)
+            logger.info(log_str)
+            error_log_buffer.clear()
+
+
+if __name__ == '__main__':
+    main()
